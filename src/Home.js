7e3c81954c49e3ca6c624da266b33289ee37e77a
yeonsu <bestdustn@sookmyung.ac.kr>
2022-08-04 16:28:59 +0900

    router 설치 및 추가

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <h2>리액트 배포 연습</h2>
+            <p>React를 GitHub pages를 이용하여 배포해봅시다..!</p>
+        </>
+    )
+}
+
+export default Home;
