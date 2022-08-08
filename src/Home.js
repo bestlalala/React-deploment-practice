a05ea5d5e65a0e6f210f404b135dfdc4d5269f3e
yeonsu <bestdustn@sookmyung.ac.kr>
2022-08-08 17:56:02 +0900

    태그 수정

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Home() {
     return (
-        <>
+        <div>
             <h2>리액트 배포 연습</h2>
             <p>React를 배포해봅시다..!</p>
-        </>
+        </div>
     )
 }
 
